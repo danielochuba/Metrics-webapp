85128399fd36592ef2b9dc0cf93cb9c7d764fc16
Daniel Ochuba <danielochuba78@gmail.com>
2023-07-12 14:12:37 +0100

    add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { IoChevronBackSharp } from 'react-icons/io5';
+
+const Navbar = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    {' '}
+    <IoChevronBackSharp />
+    {' '}
+  </nav>
+);
+
+export default Navbar;
