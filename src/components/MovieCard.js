913eea04333b026aecf8ab3fe2cb7460a28da780
Daniel Ochuba <danielochuba78@gmail.com>
2023-07-13 23:55:34 +0100

    add structure of each card and map the movie array

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Col, Badge } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import '../css/movieCard.css';
 
-const MovieCard = () => (
-  <div>Card</div>
-);
+function MovieCard() {
+  const dataItems = useSelector((store) => store.movies.movies);
+  const mappedMovies = dataItems.map((movie) => (
+    <Col className="movieCard" key={movie.id} xs={6} sm={6} md={4} lg={3}>
+      <Link key={movie.id} to={`/details/${movie.id}`}>
+        <img className="cardImage" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+        <Badge className="badge" bg="secondary">{ movie.title }</Badge>
+      </Link>
+    </Col>
+  ));
+
+  return (
+    <>
+      {mappedMovies}
+    </>
+  );
+}
 
 export default MovieCard;
